57e271c8c2b757976bf366b3233ec0f6e723581b
Christoph Becher <christoph.becher@me.com>
2015-01-24 02:15:24 +0100

    update

--- Octopussy/credits.ts
+++ Octopussy/credits.ts
@@ -3,10 +3,25 @@
 module Octopussy {
     export class Credits extends FullscreenState {
 
+    	private credits = "Credits\nFoo\nFoo\nFoo\nFoo\nFoo\nFoo\nFoo\nFoo\nFoo";
+
         create() {
             super.create();
 
             this.stage.setBackgroundColor('#99EE24');
+
+            var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+            var text = this.add.text(this.game.world.centerX, 0, this.credits, style);
+            text.anchor.x = Math.round(text.width * 0.5) / text.width;
+            text.anchor.y = 1;
+
+            var tween = this.add.tween(text).to({ y: 2000 }, 30000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.changeState, this);
+        }
+
+        private changeState() {
+
+        	this.game.state.start('MainMenu');
         }
     }
 } 
--- Octopussy/intro.ts
+++ Octopussy/intro.ts
@@ -3,6 +3,8 @@
 module Octopussy {
     export class Intro extends FullscreenState {
 
+        private logo: Phaser.Sprite;
+
         preload() {
 
             this.load.image('logo_cupworks', 'assets/logo_cupworks.png');
@@ -11,12 +13,21 @@ module Octopussy {
         create() {
             super.create();
 
-            var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo_cupworks');
-            logo.anchor.setTo(0.5, 0.5);
-            logo.alpha = 0;
+            this.logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo_cupworks');
+            this.logo.anchor.setTo(0.5, 0.5);
+            this.logo.alpha = 0;
 
-            var tween = this.add.tween(logo).to({ alpha: 1 }, 2000, Phaser.Easing.Bounce.InOut, true);
+            var tween = this.add.tween(this.logo).to({ alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.fateOut, this);
+        }
+
+        private fateOut() {
+
+            var tween = this.add.tween(this.logo).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(this.changeState, this);
+            //var timer = this.game.time.create(true);
+            //timer.add(2000, this.changeState, this);
+            //timer.start();
         }
 
         private changeState() {
