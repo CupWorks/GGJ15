8160d837e158efaab4033347e647aa16aac2679f
JunkieXL1983 <mikegehrhardtprivat@gemMBP.local>
2015-01-24 18:44:33 +0100

    -wip
    stuff

--- Octopussy/level.ts
+++ Octopussy/level.ts
@@ -36,14 +36,22 @@ module Octopussy {
         player: Phaser.Sprite;
         levelMusic: Phaser.Sound;
         hud: Phaser.Sprite;
+        lives: number = 7;
+        liveSpritegroup: any;
 
         preload() {
 
             this.load.image('tile_block', 'assets/level/tile_block.png');
             this.load.image('tile_way', 'assets/level/tile_way.png');
 
-            //this.load.image('hud','assets/level1/interface_mock_1.png');
-            //this.load.spritesheet('player', 'assets/level1/Sprite_2500x500er.png', 500, 500);
+            this.load.image('hud_mask','assets/level/hud/mask_hud_main.png');
+            this.load.image('hud_btn_left','assets/level/hud/btn_hud_left.png');
+            this.load.image('hud_btn_right','assets/level/hud/btn_hud_right.png');
+            this.load.image('hud_btn_up','assets/level/hud/btn_hud_up.png');
+            this.load.image('hud_btn_down','assets/level/hud/btn_hud_down.png');
+            this.load.image('ico_hud_live','assets/level/hud/ico_hud_live.png');
+
+            this.load.spritesheet('player', 'assets/tile_player_move.png', 512, 512);
             //this.load.audio('level1_background','assets/level1/sounds/octodwarfs_sad2.mp3');
 
         }
@@ -181,8 +189,8 @@ module Octopussy {
 
             this.stage.setBackgroundColor('#FFFFFF');
 
-            //this.player = this.initPlayer();
-            //this.initHud();
+            this.player = this.initPlayer();
+            this.initHud();
             //this.initSound();
 
         }
@@ -195,20 +203,21 @@ module Octopussy {
 
         initHud() {
 
-            this.hud = this.add.sprite(0, 0, 'hud');
+            this.hud = this.add.sprite(0, 0, 'hud_mask');
             this.hud.fixedToCamera = true;
         }
 
         initPlayer() {
 
-            var player = this.add.sprite(730, 320, 'player');
-            player.scale.setTo(0.8, 0.8);
+            var player = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+            player.height = this.tileSize*1.5;
+            player.width = this.tileSize*1.5;
             player.anchor.setTo(0.5, 0.5);
-            player.animations.add('up', [0], 10, false);
-            player.animations.add('down', [1], 10, false);
-            player.animations.add('waiting', [2], 10, false);
-            player.animations.add('right', [3], 10, false);
-            player.animations.add('left', [4], 10, false);
+            player.animations.add('up', [18,19,20,21,22,23], 10, false);
+            player.animations.add('down', [12,13,14,15,16,17], 10, false);
+            player.animations.add('waiting', [0,1,2], 10, false);
+            player.animations.add('right', [6,7,8,9,10,11], 10, false);
+            player.animations.add('left', [24,25,26,27,28,29], 10, false);
             player.animations.play('waiting');
 
             return player;
@@ -247,6 +256,7 @@ module Octopussy {
 
             if(this.canMove(Direction.Left)) {
 
+                this.player.animations.play('left');
                 this.startUpdatePosition(Direction.Left);
             }
         }  
@@ -255,6 +265,7 @@ module Octopussy {
 
             if(this.canMove(Direction.Right)) {
 
+                this.player.animations.play('right');
                 this.startUpdatePosition(Direction.Right);
             }
         }   
@@ -263,6 +274,7 @@ module Octopussy {
 
             if(this.canMove(Direction.Up)) {
 
+                this.player.animations.play('up');
                 this.startUpdatePosition(Direction.Up);
             }
         }  
@@ -271,6 +283,7 @@ module Octopussy {
 
             if(this.canMove(Direction.Down)) {
 
+                this.player.animations.play('down');
                 this.startUpdatePosition(Direction.Down);
             }
         }   
