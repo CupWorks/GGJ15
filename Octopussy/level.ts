862ff7d1ad2429563c664e1454199a219a6d0ce4
Christoph Becher <christoph.becher@me.com>
2015-01-25 13:45:25 +0100

    update

--- Octopussy/level.ts
+++ Octopussy/level.ts
@@ -12,7 +12,7 @@ module Octopussy {
         private tileSize: number =  180;
         private levelData: LevelData = new LevelData();
         private currentLevel: number = 0;
-        private currentLevelData: string[];
+        private currentLevelData: string[] = this.levelData.get(0);
         private visibleTiles: Phaser.Sprite[][] = 
         [
             [null, null, null, null, null],
